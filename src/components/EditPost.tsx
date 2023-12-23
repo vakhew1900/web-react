533870adb4bc0768f16dc38ee0d4fc649844c18a
Чупинин Антон <chupinin@intervolga.ru>
2023-12-23 17:07:50 +0300

    add update

--- src/components/EditPost.tsx
+++ src/components/EditPost.tsx
@@ -1,78 +1,105 @@
-import { Box, Button, TextField } from "@mui/material"
-import useSetPosts from "../hooks/useSetPosts"
-import { useRef } from "react"
-import { Post } from "../types"
-
+import { Box, Button, TextField } from "@mui/material";
+import useSetPosts from "../hooks/useSetPosts";
+import { useRef } from "react";
+import { Post } from "../types";
+import { server_address } from "../server_adress";
 
 type Props = {
-    post: Post,
-    setIsRedact : React.Dispatch<React.SetStateAction<boolean>>
-}
-
+  post: Post;
+  setIsRedact: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export default function EditPost({ post, setIsRedact }: Props) {
-    const setPosts = useSetPosts()
-    const inputRef = useRef<HTMLInputElement>(null)
-    const titleRef = useRef(post.title)
-    const bodyRef = useRef(post.body)
-    return (
-        <Box component={'form'} onSubmit={e => {
-            e.preventDefault()
-            const formData = new FormData(e.currentTarget)
-            const title = formData.get('title') as string
-            const body = formData.get('body') as string
+  const setPosts = useSetPosts();
+  const inputRef = useRef<HTMLInputElement>(null);
 
-            
-            if (title && body) {
-                if (inputRef.current) inputRef.current.value = ''
-                
-                setPosts?.((prevPosts) => {
-                    const newPosts = prevPosts.map((t) => {
-                        if (t.id === post.id) {
-                            return {
-                                ...t,
-                                title: title,
-                                body: body 
-                            }
-                        }
-                        return t
-                    }
-                    )
+  return (
+    <Box
+      component={"form"}
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const title = formData.get("title") as string;
+        const body = formData.get("body") as string;
+        const id = formData.get("id") as string;
+        const userId = formData.get("userId") as string;
 
-                    return newPosts;
-                })
-                setIsRedact?.(false)
-            }
+        if (title && body) {
+          if (inputRef.current) inputRef.current.value = "";
+
+          const newPost = {
+            title: title,
+            body: body,
+            like: false,
+            id: id,
+            userId: userId,
+          };
+          fetch(server_address + "/api/posts", {
+            method: "PUT",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify({ post: newPost }),
+          })
+            .then((response) => response.json())
+            .then((json) => {
+              if (json.message != undefined) {
+                alert(json.message);
+                return;
+              }
+
+              setPosts?.((prevPosts) => {
+                const newPosts = prevPosts.map((t) => {
+                  if (t.id === post.id) {
+                    return {
+                      ...t,
+                      title: title,
+                      body: body,
+                    };
+                  }
+                  return t;
+                });
+
+                return newPosts;
+              });
+            });
+
+          setIsRedact?.(false);
         }
-        }
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                my: 2
-            }}
-        >
-            <TextField
-                variant='filled'
-                name='title'
-                defaultValue={post.title}
-                placeholder='post title'
-                inputRef={inputRef}
-                color={'success'}
-            />
+      }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        my: 2,
+      }}
+    >
+      <input type="hidden" name="id" value={post.id} />
+      <input type="hidden" name="userId" value={post.userId} />
 
-            <TextField
-                variant='filled'
-                name='body'
-                defaultValue={post.body}
-                inputRef={inputRef}
-                multiline
-                rows={4}
-                color={'success'}
-            />
+      <TextField
+        variant="filled"
+        name="title"
+        defaultValue={post.title}
+        placeholder="post title"
+        inputRef={inputRef}
+        color={"success"}
+      />
 
-            <Button type='submit' color={'success'}>Edit Post</Button>
-        </Box>
-    )
+      <TextField
+        variant="filled"
+        name="body"
+        defaultValue={post.body}
+        inputRef={inputRef}
+        multiline
+        rows={4}
+        color={"success"}
+      />
 
+      <Button type="submit" color={"success"}>
+        Edit Post
+      </Button>
+    </Box>
+  );
 }
